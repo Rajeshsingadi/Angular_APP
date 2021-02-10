de4c96b7d2ba884b7a63569037f869a88ebaddce
Rajeshsingadi <rajeshkumarraje9@gmail.com>
2021-02-10 23:07:01 +0530

    Angular Project

---
+++ src/shared/models/budget-item.model.ts
@@ -0,0 +1,11 @@
+export class BudgetItem{
+    //description: string;
+    //amount: number;
+    //giving in single line parameter in TS
+
+    constructor(public description: string, public amount: number){
+        //this.description=description;
+        //this.amount=amount;
+
+    }
+}
